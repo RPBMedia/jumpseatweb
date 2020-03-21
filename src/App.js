7e62eaed50c176620fecb66431ae853db1cbc7ff
Rui Baiao <rui.palma.baiao@gmail.com>
2020-03-21 16:18:31 +0100

    home bootstrap. dependencies added

--- src/App.js
+++ src/App.js
@@ -2,24 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from "firebase/app";
+import "firebase/auth";
+
+import config from './config';
+
+import Home from './views/Home';
+
 function App() {
+  console.log(config.firebaseConfig);
+  debugger;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <FirebaseAuthProvider firebase={firebase} {...config}>
+        <Home />
+      </FirebaseAuthProvider> 
+    </>
   );
 }
 
---
+++ src/config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "API_KEY",
+  projectId: "jump-seat-cb881",
+  databaseURL: "DATABASE_URL",
+  authDomain: "AUTH_DOMAIN",
+  // OPTIONAL
+  storageBucket: "STORAGE_BUCKET",
+  messagingSenderId: "MESSAGING_SENDER_ID"
+};
+
+export default {
+  firebaseConfig
+};
